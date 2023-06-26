b036a340b44c297ddf971f3536fbe85d674960fd
MarwanOladele <oladelemarwan@gmail.com>
2023-06-26 10:59:30 +0100

    client side initial setip

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="bg-primary h-screen ">
+      <div>
+        <h1>Marwan MP</h1>
+        <Button type="primary">AntD 1st Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
