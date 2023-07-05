f3532e3f93174820866e1e018594aa5cddff5baa
MarwanOladele <oladelemarwan@gmail.com>
2023-07-05 16:31:01 +0100

    Register Component Implemented

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -5,9 +5,9 @@ import Divider from "../../components/DIvider";
 const rules = [
   {
     required: true,
-    message: 'required',
-  }
-]
+    message: "required",
+  },
+];
 
 const Register = () => {
   const onFinish = (values) => {
@@ -15,27 +15,27 @@ const Register = () => {
   };
   return (
     <div className="h-screen bg-primary flex justify-center items-center">
-      <div className="bg-white p-5 rounded w-[450px]">
+      <div className="bg-white p-5 rounded w-[350px]">
         <h1 className="text-primary text-2xl">
           MMP - <span className="text-gray-400">Register</span>
         </h1>
         <Divider />
         <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item label="Name" name="name">
+          <Form.Item label="Name" name="name" rules={rules}>
             <Input placeholder="name" />
           </Form.Item>
-          <Form.Item label="Email" name="email">
+          <Form.Item label="Email" name="email" rules={rules}>
             <Input placeholder="email" />
           </Form.Item>
-          <Form.Item label="Password" name="password">
+          <Form.Item label="Password" name="password" rules={rules}>
             <Input type="password" placeholder="password" />
           </Form.Item>
-          <Button type="primary" htmlType="submit" block>
+          <Button type="primary" htmlType="submit" block className="mt-2">
             Register
           </Button>
           <div className="mt-5 text-center">
             <span className="text-gray-500">
-              Already have an account?{" "}
+              Already have an account?
               <Link to="/login" className="text-primary">
                 Login
               </Link>
